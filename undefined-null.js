70122e0a6f08d8cfc64336703c2b8bddd1475b68
Sunny <shubelislam.diu@gmail.com>
2021-09-01 10:23:38 +0600

    undefined vs null

---
+++ undefined-null.js
@@ -0,0 +1,39 @@
+let first;
+// console.log(first);
+function second(x, y) {
+    // document.getElementById('sum');
+
+}
+const result = second(3, 91);
+// console.log(result);
+function add(a, b) {
+    const sum = a + b;
+
+    if (b < 10) {
+        return;
+    }
+    const fun = a * b;
+    return sum;
+}
+
+const fourth = add(2, 7);
+// console.log(fourth)
+function double(a, b) {
+    const result = a * 2;
+    // console.log(b);
+    return result;
+}
+double(81);
+
+const fifth = { name: 'Jakir', age: 15, location: 'Dhaka' };
+// console.log(fifth.phone);
+
+const sixth = [24, 25, 61, 91];
+// console.log(sixth[4])
+delete sixth[2];
+// console.log(sixth[2]);
+
+const seventh = undefined;
+// console.log(seventh);
+
+const myObj = { name: 'Nabil', profession: null };
